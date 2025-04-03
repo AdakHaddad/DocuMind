7523f4b3e94d7f8dfcc7e20df947d1068726c931
bluefl4mingo <ramadhanifebruarrahman@mail.ugm.ac.id>
2025-04-04 05:39:35 +0700

    Added Flashcard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,6 @@
 "use client";
 
 import React from "react";
-<<<<<<< HEAD
-import Footer from "../components/layout/Footer";
-import Header from "../components/layout/Header";
-
-const Home = () => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-black">
-      <h1 className="text-4xl font-bold mb-6 font-handwritten">DocuMind</h1>
-      <h1 className="text-2xl font-light mb-6 font-handwritten">
-        Platform Pembelajaran Efisien
-      </h1>
-
-      <div className="flex flex-col gap-3">
-        <button className="bg-black text-white px-6 py-2 rounded-md text-lg">
-          Sign In
-        </button>
-        <button className="border-2 border-black text-black px-6 py-2 rounded-md text-lg">
-          Sign Up
-        </button>
-      </div>
-
-      <div className="absolute bottom-6 flex gap-4">
-        <button className="bg-black text-white px-4 py-2 rounded-md text-sm">
-          Contact
-        </button>
-        <button className="bg-black text-white px-4 py-2 rounded-md text-sm">
-          About Us
-        </button>
-        <button className="bg-black text-white px-4 py-2 rounded-md text-sm">
-          Privacy Policy
-        </button>
-      </div>
-      <Footer />
-=======
 
 const Home = () => {
   return (
@@ -52,7 +18,6 @@ const Home = () => {
           {"<welcome>"}
         </div>
       </div>
->>>>>>> b3116f6cfa86f83fee71b45b75d25fd4c07fd2f0
     </div>
   );
 };
