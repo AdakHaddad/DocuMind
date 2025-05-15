5b80a9b1c8fe096cd5f40409b34f6dd2e3c52339
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-05-16 03:29:39 +0700

    feat regenerate prompt modal

--- src/app/(guest)/test/page.tsx
+++ src/app/(guest)/test/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import AIRoomChatModal from "@/src/components/AIRoomChatModal";
+import RegeneratePromptModal from "@/src/components/RegeneratePromptModal";
 import React from "react";
 
 const Test = () => {
   return (
     <div className="flex flex-col w-full h-full items-center justify-center relative overflow-clip">
-      <AIRoomChatModal />
+      <RegeneratePromptModal />
       <h1>Lorem Ipsum</h1>
     </div>
   );
